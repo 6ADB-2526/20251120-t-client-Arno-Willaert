f6e3c66882f127f87da79b7574a7ac142efbd083
Arno-Willaert <Arno.willaert@student.tsaam.be>
2025-11-21 12:12:21 +0100

    commit2

--- script/script.js
+++ script/script.js
@@ -3,6 +3,9 @@
  */
 
 const lijstKinderen = document.querySelector("#kinderen")
+const geschenklijst = document.querySelector("#geschenkenlijst")
+
+const addKind = document.querySelector("#addKind")
 
 fetch("https://o-apiandclient-render.onrender.com/kinderen")
     .then(info => info.json())
@@ -11,7 +14,28 @@ fetch("https://o-apiandclient-render.onrender.com/kinderen")
         kinderen.forEach((kind) => {
             const newOption = document.createElement("option");
             newOption.value = kind.id
-            newOption.innerHTML = kind.
+            newOption.innerHTML = kind.voornaam
             lijstKinderen.appendChild(newOption)
         });
     })
+
+lijstKinderen.addEventListener("change", (e) => {
+    selectedID = e.target.value
+
+    fetch("https://o-apiandclient-render.onrender.com/kinderen")
+    .then(info => info.json())
+    .then((kinderen) =>  {
+        kinderen.forEach((geschenk) => {
+            geschenklijst.innerHTML = ""
+            const newList = document.createElement("li")
+            newList.innerHTML = geschenk.fullGeschenk
+            console.log(newList);
+            geschenklijst.appendChild(newList)
+        })        
+    })
+})
+
+
+const btn = document.createElement("button")
+btn.innerHTML = "voeg toe"
+addKind.appendChild(btn)
