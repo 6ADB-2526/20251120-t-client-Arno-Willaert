931267063d059bf0a62b1cea3e0037bb466d2a16
Arno-Willaert <Arno.willaert@student.tsaam.be>
2025-11-21 11:52:59 +0100

    commit1

--- script/script.js
+++ script/script.js
@@ -1,3 +1,17 @@
 /**
  * 
  */
+
+const lijstKinderen = document.querySelector("#kinderen")
+
+fetch("https://o-apiandclient-render.onrender.com/kinderen")
+    .then(info => info.json())
+    .then((kinderen) => {
+        console.log(kinderen);
+        kinderen.forEach((kind) => {
+            const newOption = document.createElement("option");
+            newOption.value = kind.id
+            newOption.innerHTML = kind.
+            lijstKinderen.appendChild(newOption)
+        });
+    })
